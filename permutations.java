fb0845acb105f17cbbfe5485466a09c0b7fc1092
sajaljain88 <101251742+sajaljain88@users.noreply.github.com>
2022-03-14 21:16:36 +0530

    add new

---
+++ permutations.java
@@ -0,0 +1,32 @@
+package sebone2;
+import java.util.*;
+
+public class permutations {
+	
+	 public void printPermutations(String str,String ans) {
+	      if(str.length()==0) {
+		     System.out.println(ans);
+		     return;
+	      }
+	    for(int i=0;i<str.length();i++) {
+		   char ch=str.charAt(i);
+		   String sl=str.substring(0,i);
+		   String sr=str.substring(i+1);
+		   String res=sl+sr;
+		   printPermutations(res,ans+ch);
+		
+	         }
+	    
+	    }
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("hy");
+		permutations p=new permutations();
+		String s=sc.next();
+		p.printPermutations(s,"");
+		
+
+	}
+
+}
